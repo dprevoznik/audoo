53308bf01879d5e6234bd3747b124fb4719b7f40
Daniel <dprevoznik@gmail.com>
2020-04-23 23:52:32 -0400

    Added dockerfile for running node process in Docker

--- server/server.js
+++ server/server.js
@@ -9,6 +9,10 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, "../build")));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build/", "index.html"));
+});
+
 app.use("/service", router);
 
 app.listen(port, () =>
