d00b57893099468dc725d3d9847f3931aa96e252
Daniel <dprevoznik@gmail.com>
2020-05-18 11:47:15 -0400

    Added right axis to graph and also am now able to transition smoothly between the three different data sets.

--- src/client/statsPage.jsx
+++ src/client/statsPage.jsx
@@ -2,16 +2,35 @@ import React, { useEffect, useState } from "react";
 import * as d3 from "d3";
 import fetchAudoos from "./fetchAudoos.js";
 
-let StatsPage = ({ page }) => {
-  let [data, setData] = useState([]);
-  let [counts, setCounts] = useState([]);
+var StatsPage = ({ page }) => {
+  var [data, setData] = useState([]);
+  var [counts, setCounts] = useState([]);
+  var [selected, setSelected] = useState("Audoos");
 
   const width = 250;
   const height = 250;
-
+  const margin = { top: 20, bottom: 20, left: 20, right: 30 };
+  const svg = d3.select("svg");
+  svg.attr("transform", `translate(25, 25)`);
+  var xScale = d3
+    .scaleBand()
+    .rangeRound([margin.left, width - margin.right], 10, 10)
+    .paddingInner(0)
+    .paddingOuter(0.5)
+    .align(0.5);
+  var yScale = d3.scaleLinear().range([height - margin.bottom, margin.top]);
+  var yAxis = d3.axisRight().scale(yScale).ticks(3);
+  var xAxis = d3.axisBottom().scale(xScale);
+  if (svg.select(".yAxis").node() === null) {
+    svg.append("g").attr("class", "xAxis");
+    svg.append("g").attr("class", "yAxis");
+  }
+  const updateSelected = (e) => {
+    setSelected(e.target.value);
+  };
   useEffect(() => {
-    fetchAudoos("Audoos", setData);
-  }, []);
+    fetchAudoos(selected, setData);
+  }, [selected]);
 
   useEffect(() => {
     if (data.length > 0) {
@@ -27,42 +46,62 @@ let StatsPage = ({ page }) => {
       for (let key in output) {
         outputArray.push({ key, count: output[key] });
       }
+      outputArray.sort((a, b) => {
+        let textA = a.key.toUpperCase();
+        let textB = b.key.toUpperCase();
+        return textA < textB ? 1 : -1;
+      });
       setCounts(outputArray);
+    } else {
+      setCounts([]);
     }
   }, [data]);
 
-  useEffect(() => {
-    const svg = d3.select("svg");
-    svg.attr("transform", "translate(25,25)");
-    let xDomain = counts.map((d) => d.key);
-    let xScale = d3.scaleBand().domain(xDomain).rangeRound([0, width], 10, 10).paddingInner(0).paddingOuter(.5).align(.5)
-    let yMax = d3.max(counts, (d) => d.count);
-    let yScale = d3.scaleLinear().domain([0, yMax + 1]).range([height, 0]);
-    let colorScale = d3.scaleOrdinal(d3.schemeCategory10);
-    let circles = svg
-      .selectAll("circle")
-      .data(counts, (d) => d.key)
+  useEffect(() => {}, []);
 
-    circles.exit().remove();
+  useEffect(() => {
+    const t = d3.transition().duration(1000);
+
+    let yMax = d3.max(counts, (d) => d.count);
+    yScale.domain([0, yMax + 1]).nice();
+
+    svg
+      .select(".yAxis")
+      .attr("transform", `translate(${width - margin.right}, 0)`)
+      .transition(t)
+      .call(yAxis);
+
+    let xDomain = counts.map((d) => d.key);
+    xScale.domain(xDomain);
+
+    let colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+    let circles = svg.selectAll("circle").data(counts, (d) => d.key);
+
+    circles.exit().transition(t).attr("r", 0).remove();
     let enter = circles
       .enter()
       .append("circle")
       .attr("cx", (d) => xScale(d.key))
       .attr("cy", (d) => yScale(d.count))
-      .attr('r', 10)
-      .style('opacity', .5);
+      .style("opacity", 0.5);
 
     enter
       .merge(circles)
       .attr("fill", (d) => colorScale(d.key))
-      .transition().duration(1000)
+      .transition(t)
+      .attr("r", 10)
       .attr("cx", (d) => xScale(d.key))
       .attr("cy", (d) => yScale(d.count));
   }, [counts]);
 
   return (
     <div>
-      <svg style={{ width: `${width}px`, height: `${height}px`, 'border': '10px solid black'}}></svg>
+      <select onChange={updateSelected}>
+        <option value="Audoos">Audoos</option>
+        <option value="Shared">Shared</option>
+        <option value="Feed">Feed</option>
+      </select>
+      <svg style={{ width: `${width}px`, height: `${height}px` }}></svg>
     </div>
   );
 };
